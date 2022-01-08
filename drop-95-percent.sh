0c7b327be6c1f675487e589e8bd11126f1a7ac0f
Piyush Dhall <contact@piyush.ovh>
2022-01-08 14:26:15 +0530

    change echo to exec

--- drop-95-percent.sh
+++ drop-95-percent.sh
@@ -2,5 +2,5 @@
 # Script to achieve 95% packetloss on a Linux host.
 # Might be useful to make some nice loss graphs for Smokeping :P
 
-echo "iptables -m statistic --mode random --probability 0.05 -j ACCEPT"
-echo "iptables -j DROP"
+exec "iptables -m statistic --mode random --probability 0.05 -j ACCEPT"
+exec "iptables -j DROP"
